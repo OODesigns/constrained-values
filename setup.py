40cf4f6ae8dd65c2317892fc950f96d7675cc0f8
Michael.lindre <michael.lindre@oodesigns.co.uk>
2025-09-15 08:49:55 +0100

    UPDATE: fix license declaration and adjust python_requires version in setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     url='https://github.com/OODesigns/constrained-values',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    license='MIT',
     python_requires='>=3.6',
 )
